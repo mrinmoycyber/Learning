c47f8650c190f9f6a17e21a27cb80420c381b51f
mrinmoycyber <mrinmoy.kar23@gmail.com>
2024-10-08 05:49:37 +0530

    Add initial content to BubbleSortTuples.py

---
+++ Tuples/BubbleSortTuples.py
@@ -0,0 +1,5 @@
+# Sort a list of tuples based on the second element of each tuple without using Python's built-in sort function
+
+def bubble_sort_tuples(tuples_list):
+    
+    pass  # TODO: implement bubble sort
